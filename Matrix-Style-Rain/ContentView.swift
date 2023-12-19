2b4d788713f48f22f0be332f435dd5b79a4612b4
Elaidzha1940 <eli080293@gmail.com>
2023-12-19 12:57:48 +0300

    Matrix_Style_RainApp

---
+++ Matrix-Style-Rain/ContentView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: Matrix-Style-Rain
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 19.12.2023
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+          
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Matrix-Style-Rain/Matrix_Style_RainApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: Matrix-Style-Rain
+//  File: Matrix_Style_RainApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 19.12.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct Matrix_Style_RainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

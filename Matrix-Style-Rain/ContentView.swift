5de7d6ebc0110a507069a8988c342717350235e8
Elaidzha1940 <eli080293@gmail.com>
2023-12-19 13:16:26 +0300

    Update ContentView

--- Matrix-Style-Rain/ContentView.swift
+++ Matrix-Style-Rain/ContentView.swift
@@ -12,8 +12,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        VStack {
-          
+        GeometryReader { proxy in
+            let size = proxy.size
+            
+            HStack(spacing: 10) {
+                ForEach(1...Int(size.width / 25), id: \.self ) { _ in
+                    
+                }
+            }
+            .padding(.horizontal)
         }
     }
 }
@@ -21,3 +28,31 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+struct Rain: View {
+    var size: CGSize
+
+    @State var startAnimation: Bool = false
+    @State var random: Int = 0
+    
+    var body: some View {
+        let randomHeight: CGFloat = .random(in: (size.height / 2)...size.height)
+        
+        VStack {
+            ForEach(0..<constant.count, id: \.self) { index in
+                let character = Array(constant)[getRandomIndex(index: index)]
+            }
+        }
+    }
+}
+
+struct getRandomIndex(index: Int) -> Int {
+    let max = constant.count - 1
+    
+    if(index + random) > max {
+        if(index + random)
+    }
+}
+
+let constant = "0900909900880900808090"
+

ebd802f818ac77fddf5becdb2790a45d2cf8a56d
Elaidzha1940 <eli080293@gmail.com>
2023-12-19 15:34:59 +0300

    Done

--- Matrix-Style-Rain/MatrixStyleRain.swift
+++ Matrix-Style-Rain/MatrixStyleRain.swift
@@ -9,14 +9,15 @@
 
 import SwiftUI
 
-struct  MatrixStyleRain: View {
+struct MatrixStyleRain: View {
     
     var body: some View {
+        
         GeometryReader { proxy in
             let size = proxy.size
             
             HStack(spacing: 10) {
-                ForEach(1...Int(size.width / 25), id: \.self ) { _ in
+                ForEach(1...Int(size.width / 20), id: \.self) { _ in
                     Rain(size: size)
                 }
             }
@@ -30,15 +31,15 @@ struct  MatrixStyleRain: View {
         @State private var startAnimation: Bool = false
         @State private var random: Int = 0
         
-        let constant = "0900909900880900808090"
-        
         var body: some View {
             let randomHeight: CGFloat = .random(in: (size.height / 2)...size.height)
             
             VStack {
-                ForEach(0..<constant.count, id: \.self) { index in
-                    let charecter = Array(constant)[getRandomIndex(index: index)]
-                    Text(String(charecter))
+                ForEach(0..<max(0, min(constant.count, 40)), id: \.self) { index in
+                    if let character = Array(constant)[safe: index] {
+                        Text(String(character))
+                            .font(.system(size: 20, weight: .bold, design: .rounded))
+                    }
                 }
             }
             .mask(alignment: .top) {
@@ -47,44 +48,37 @@ struct  MatrixStyleRain: View {
                         LinearGradient(colors: [
                             .clear,
                             .black.opacity(0.1),
-                            .black.opacity(0.3),
-                            .black.opacity(0.5),
+                            .black.opacity(0.4),
                             .black.opacity(0.7),
-                            .black
+                            .gray,
                         ], startPoint: .top, endPoint: .bottom)
                     )
                     .frame(height: size.height / 2)
                     .offset(y: startAnimation ? size.height : -randomHeight)
             }
             .onAppear {
-                withAnimation(.linear(duration: 2).delay(.random(in: 0...2)).repeatForever(autoreverses: false)) {
+                withAnimation(Animation.linear(duration: 2).delay(.random(in: 0...2)).repeatForever(autoreverses: false)) {
                     startAnimation = true
                 }
             }
-            .onReceive(Timer.publish(every: 0.2, on: .main, in: .common).autoconnect()) { _ in
+            .onReceive(Timer.publish(every: 0.3, on: .main, in: .common).autoconnect()) { _ in
                 random = Int.random(in: 0..<constant.count)
-            }
-        }
-        func getRandomIndex(index: Int) -> Int {
-            let max = constant.count - 1
-            
-            if (index + random) > max {
-                if (index + random) < 0 {
-                    return index
-                }
-                return (index - random)
-            } else {
-                return (index + random)
             }
         }
     }
 }
 
-#Preview {
-    MatrixStyleRain()
-        .preferredColorScheme(.dark)
+struct MatrixStyleRain_Previews: PreviewProvider {
+    static var previews: some View {
+        MatrixStyleRain()
+            .preferredColorScheme(.dark)
+    }
 }
 
+let constant = "3.141592653589793 3.141592653589793"
 
-
-
+extension Array {
+    subscript(safe index: Index) -> Element? {
+        indices.contains(index) ? self[index] : nil
+    }
+}

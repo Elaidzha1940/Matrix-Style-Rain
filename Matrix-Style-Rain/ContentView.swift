36f5b65d7c2a5119fd3c0365d64a28419e9617fa
Elaidzha1940 <eli080293@gmail.com>
2023-12-19 13:41:47 +0300

    Add struct Rain

--- Matrix-Style-Rain/ContentView.swift
+++ Matrix-Style-Rain/ContentView.swift
@@ -11,13 +11,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
         GeometryReader { proxy in
             let size = proxy.size
             
             HStack(spacing: 10) {
                 ForEach(1...Int(size.width / 25), id: \.self ) { _ in
-                    
+                    Rain(size: size)
                 }
             }
             .padding(.horizontal)
@@ -25,26 +24,23 @@ struct ContentView: View {
     }
 }
 
-#Preview {
-    ContentView()
-}
-
 struct Rain: View {
     var size: CGSize
     
-    @State var startAnimation: Bool = false
-    @State var random: Int = 0
+    @State private var startAnimation: Bool = false
+    @State private var random: Int = 0
     
     var body: some View {
         let randomHeight: CGFloat = .random(in: (size.height / 2)...size.height)
         
         VStack {
-            
+            ForEach(0..<constant.count, id: \.self) { index in
+                let character = Array(constant)[getRandomIndex(index: index)]
+                Text(String(character))
+            }
         }
-        .mask(alignment: .top) {
-            Rectangle()
-                .fill()
-            LinearGradient(colors: [
+        .background(
+            LinearGradient(gradient: Gradient(colors: [
                 .clear,
                 .black.opacity(0.1),
                 .black.opacity(0.2),
@@ -52,38 +48,39 @@ struct Rain: View {
                 .black.opacity(0.4),
                 .black.opacity(0.5),
                 .black
-            ],
-                           startPoint: .top,
-                           endPoint: .bottom)
+            ]),
+            startPoint: .top,
+            endPoint: .bottom)
             .frame(height: size.height / 2)
             .offset(y: startAnimation ? size.height : -randomHeight)
-            
-        }
+        )
         .onAppear {
             withAnimation(.linear(duration: 2).delay(.random(in: 0...2)).repeatForever(autoreverses: false)) {
-                
+                startAnimation = true
             }
         }
-    }
-}
-
-func getRandomIndex(index: Int) -> Int {
-    let max = constant.count - 1
-    
-    if(index + random) > max {
-        if(index + random) < 0 {
-            return index
+        .onReceive(Timer.publish(every: 0.3, on: .main, in: .common).autoconnect()) { _ in
+            random = Int.random(in: 0..<constant.count)
         }
-        return(index - random)
-    } else {
-        return(index + random)
+    }
+    
+    func getRandomIndex(index: Int) -> Int {
+        let max = constant.count - 1
         
+        if (index + random) > max {
+            if (index + random) < 0 {
+                return index
+            }
+            return (index - random)
+        } else {
+            return (index + random)
+        }
     }
 }
 
 let constant = "0900909900880900808090"
 
-//ForEach(0..<constant.count, id: \.self) { index in
-//    let character = Array(constant)[getRandomIndex(index: index)]
-//    Text(String(character))
-//}
+#Preview {
+    ContentView()
+        .preferredColorScheme(.dark)
+}
